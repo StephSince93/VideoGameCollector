2e631d6bb8f5556b5cb4419c9e218c845309488e
Stephen Romero <stephen.romero93@gmail.com>
2017-02-01 20:30:18 -0600

    Initial Commit

---
+++ VideoGameCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoGameCollector
+//
+//  Created by Stephen Romero on 2/1/17.
+//  Copyright © 2017 Stephen Romero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
